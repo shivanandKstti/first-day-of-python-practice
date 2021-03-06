a9a32727ad8d3486cc15f65e8c6c3ecffd33859e
shivusoumya <shivadatacamp@gmail.com>
2021-03-06 22:10:42 +0530

    python bsic if and looping concept practice

---
+++ app.py
@@ -0,0 +1,108 @@
+# students_count = 1000
+# rating = 4.99
+# is_published = True
+# course_name = "Python programming"
+
+
+# print(len(course))
+
+# print(course[0], course[-1])
+# print(course[9:])
+
+# course = "python programmng"
+# print(course.upper())
+# # print(course)
+
+# first_name = 'shiva'
+# last_name = 'katti'
+# # full_name = first_name + + last_name
+# full_name = f"{first_name} {last_name}"
+# print(full_name)
+# print(full_name.upper())
+# print(full_name.lower())
+# print(full_name.title())
+# print(full_name.strip())
+# print(full_name.find("ti"))
+# print(full_name.replace("ti", "TI"))
+# print("shiv" in full_name)
+# print("shiv" not in full_name)
+ 
+# x = 1 
+# x = 1.1
+# x = 1 + 2j 
+
+# print(round(2.9))
+
+# print(round(2.6))
+
+# x = input("x: ")
+# y = int(x) + 1
+# print(y)
+# temperatue = 25
+# if temperatue > 30:
+#     print('very hot')
+# elif temperatue > 20:
+#     print('its nice')
+# else:
+#     print('normal day')
+
+# age = 22
+# # if age >= 18:
+# #     print("Eligible")
+# # else:
+# #     print('not eligible')
+
+# message = age
+
+# and or not
+
+# high_income = True
+# good_credit = True
+
+# if high_income and good_credit:
+#     print("Eligible")
+# else: 
+#     print("not eligible")
+    
+# for message in range(1, 10, 2):
+#     print((message + 1) * ".")
+# successful = False
+# for message in range(3):
+#     print("Attempt")
+#     if successful:
+#         print("successful")
+#         break
+# else:
+#     print('Attempted 3 times and failed:(')
+        
+# for x in range(5):
+#     for y in range(3):
+#         print(f"({x}, {y})")
+     
+# print(type(5))
+# print(type(range(5))) # iterable, 
+# for x in "Siddarayajja":
+#     print(x.upper())
+    
+
+# numner = 100
+# while numner > 0:
+#     print(numner)
+#     numner //= 2
+
+# command = ""
+# while command.lower() != "quit":
+# while True:
+#     command = input(">")
+#     print("ECHO", command)
+#     if command.lower() == 'quit':
+#         break
+count = 0
+for x in range(1, 10):
+    if x % 2 == 0:
+        count += 1
+        print(x)
+print(f"we have {count} even numbers")
+        
+    
+        
